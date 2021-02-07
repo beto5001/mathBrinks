5a1847e74a9a8c8731e2cfbe61c84a3bfbc0a1c9
Alberto Ferreira da Silva <alberto.abfs@hotmail.com>
2021-02-06 23:20:24 -0300

    Criação da base de WepApi

---
+++ math.WebApi/Controllers/OperationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace math.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+    }
+}
