9ae0c04c80a7b6ad910c423108dd83c2433020ae
Alberto Ferreira da Silva <alberto.abfs@hotmail.com>
2021-02-07 00:03:40 -0300

    Criação do projeto domain

---
+++ math.Domain/Models/Operation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace math.Domain.Models
+{
+    public class Operation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
